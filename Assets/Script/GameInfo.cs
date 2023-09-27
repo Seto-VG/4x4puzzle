958f9ef05d1ae9b64dc98dd0c57944964e78ebcc
harada1152 <HUMANayu12615mu@gmail.com>
2023-09-27 22:35:01 +0900

    [add]各シーンにボタンを配置

---
+++ Assets/Script/GameInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInfo : MonoBehaviour
+{
+
+    public GameObject InfoPanel;
+    //ƒ{ƒ^ƒ“‚ğ‰Ÿ‚³‚ê‚½‚Æ‚«î•ñ‰æ–Ê‚ğ•\¦‚·‚é
+    public void OnClick()
+    {
+        
+        InfoPanel.SetActive(true);
+    }
+
+}
